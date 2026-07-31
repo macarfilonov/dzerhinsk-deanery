6b9c21a5a7deafeaf3ddfb5182cbc9f262bcba42
macarfilonov <filonovmakar12@gmail.com>
2026-07-31 19:49:00 +0300

    Добавлены все файлы сайта

--- netlify/functions/yandex-ai.js
+++ netlify/functions/yandex-ai.js
@@ -23,9 +23,9 @@ exports.handler = async (event) => {
             return { statusCode: 400, headers, body: JSON.stringify({ error: 'Вопрос не может быть пустым.' }) };
         }
 
-        const FOLDER_ID = 'b1grgek3bgbqc3vagodq';
+        const FOLDER_ID = 'ajeros54qehbjkoicb95';
 
-        const API_KEY = 'AQVN0Yi2ZXsQBGxx0qGOW4Gmm5ptMWdhl3yD-gIQ';
+        const API_KEY = 'AQVN28cKTTornTEmLSU8R4mcQks6ThmG_JT7zkd-';
 
         const response = await fetch('https://llm.api.cloud.yandex.net/foundationModels/v1/completion', {
             method: 'POST',
